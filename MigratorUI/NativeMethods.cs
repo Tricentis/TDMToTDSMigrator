8830628e34a7a8fff0effd9feade14319c8ebbd1
Kerstin Kohout <kerko@tricentis.com>
2018-12-10 10:47:44 +0100

    reformatting

--- MigratorUI/NativeMethods.cs
+++ MigratorUI/NativeMethods.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
+namespace MigratorUI {
+    internal class NativeMethods {
+        #region Constants
 
-namespace MigratorUI
-{
-    internal class NativeMethods
-    {
         public const int HWND_BROADCAST = 0xffff;
 
+        #endregion
+
+        #region Static Fields
+
         public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
+
+        #endregion
+
+        #region Public Methods and Operators
 
         [DllImport("user32")]
         public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
 
         [DllImport("user32")]
         public static extern int RegisterWindowMessage(string message);
+
+        #endregion
     }
 }
